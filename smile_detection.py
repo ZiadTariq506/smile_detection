fad4ddbf1d2d73e9ec1896066e51bd40311a96a0
ZiadTariq506 <82957280+ZiadTariq506@users.noreply.github.com>
2022-05-10 04:26:05 +0200

    Update smile_detection.py
    make it able to detect more than one face per image.

--- smile_detection.py
+++ smile_detection.py
@@ -1,9 +1,10 @@
 import cv2
+from random import randrange
 #                                   our trained AI module
 module = cv2.CascadeClassifier("module.xml")
 
 # Load image to our script
-img = cv2.imread("Rob.jpg")
+img = cv2.imread("2.webp")
 
 # convert it to gray
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -12,13 +13,14 @@ gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # detect faces
 face_coordinates = module.detectMultiScale(gray_img)
 print("there it is", face_coordinates)
-(x, y, w, h) = face_coordinates[0]
-# draw the rectangles around the face
-#            image   the upper left the bottom right rectangles s' color thick
-cv2.rectangle(img, (x, y), (x+w, y+h), (128, 0, 128), 2)
+# draw on all the faces
+for (x, y, w, h) in face_coordinates:
+    # draw the rectangles around the face
+    # image the upper left the bottom right rectangles s' color thick
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
 
 # show the image
 cv2.imshow("a Gray Image", img)
 # close it when we press any key
 cv2.waitKey()
-print("Keep Going")
+print("script is done :)")
